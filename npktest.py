0e9941e5513263d2518bf31a8d8ecc5d3d9308b6
Park Hyunwoo <ez@smartstudy.co.kr>
2013-02-06 05:43:58 +0900

    Update npktest.py

--- npktest.py
+++ npktest.py
@@ -2,17 +2,20 @@ from pytest import raises
 import npk
 
 
+filename = "testres/sample.npk"
+key = (98521, 16322, 7163, 992)
+
 class TestNpk(object):
     def test_open_package(self):
-        pack = npk.package("testres/sample.npk", (98521, 16322, 7163, 992))
+        pack = npk.package(filename, key)
         pack.close()
 
     def test_open_package_fail(self):
         with raises(npk.FailToOpenPackage):
-            pack = npk.package("testres/sample.npk", (0, 0, 0, 0))
+            pack = npk.package(filename, reversed(key))
 
     def test_iterate_entities(self):
-        pack = npk.package("testres/sample.npk", (98521, 16322, 7163, 992))
+        pack = npk.package(filename, key)
         entities = pack.all()
         entities_expected = ['sample.txt', 'tea.txt', 'zip.txt', 'zipntea.txt']
         assert len(entities) == 4
@@ -20,13 +23,13 @@ class TestNpk(object):
         pack.close()
 
     def test_get_entity(self, tmpdir):
-        pack = npk.package("testres/sample.npk", (98521, 16322, 7163, 992))
+        pack = npk.package(filename, key)
         for entity in pack.all():
             assert entity.read() == open("testres/sample.txt").read()
         pack.close()
 
     def test_get_entity_fail(self):
-        pack = npk.package("testres/sample.npk", (98521, 16322, 7163, 992))
+        pack = npk.package(filename, key)
         with raises(npk.EntityNotFound):
             pack.get("notfound.42")
         pack.close()
